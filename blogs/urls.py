95e83dcaa073dd7bbc53cab5afce24a0e9e3db79
Ayush Gautam <ayushstudy647@gmail.com>
2026-05-18 10:00:00 +0530

    Blogging-System

---
+++ blogs/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include
+
+urlpatterns = [
+      path('<int:category_id>/',views.posts_by_category,name='posts_by_category'),
+]
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Category
+from .models import Blog
+from django.http import HttpResponse
+
+def posts_by_category(request,category_id):
+      category=Category.objects.get(id=category_id)
+      posts=Blog.objects.filter(category=category,status='published')
+      context={
+            'category':category,
+            'posts':posts,
+      }
+      return render(request,'posts_by_category.html',context)
