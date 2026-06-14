daec4f0e69adb1c20c5ceacfbb443e91cb671e65
Ayush Gautam <ayushstudy647@gmail.com>
2026-06-14 10:00:00 +0530

    Blogging-System

--- blog_main/views.py
+++ blog_main/views.py
@@ -6,11 +6,10 @@ from blogs.models import Blog
 
 
 def home(request):
-      categories=Category.objects.all()
       featured_posts=Blog.objects.filter(is_featured=True,status="published")
       no_featured_posts=Blog.objects.filter(is_featured=False,status="published")
       print(no_featured_posts)
 
 
-      context={'categories':categories,'featured_posts':featured_posts,'no_featured_posts':no_featured_posts}
+      context={'featured_posts':featured_posts,'no_featured_posts':no_featured_posts}
       return render(request,'home.html',context)
